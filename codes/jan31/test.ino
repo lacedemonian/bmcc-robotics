8ccb92fa08f9e2cf443c472d259843695e979c40
mcterse <thepaveljavornik@gmail.com>
2016-01-31 15:52:01 -0500

    Create test.ino

---
+++ codes/jan31/test.ino
@@ -0,0 +1,427 @@
+#include <QTRSensors.h> // used to read the QTR Sensors
+#include <LiquidCrystal.h> // used to operate the LCD on the controller
+
+#define NUM_SENSORS   8     // number of sensors used to follow line
+#define NUM_SENSORSA  2    // number of sensors used to find cross
+#define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
+#define EMITTER_PIN   2     // emitter is controlled by digital pin 2
+
+QTRSensorsRC qtrrc((unsigned char[]) { A8, A9, A10, A11, A12, A13, A14, A15}, // six middle sensors used to follow a line
+  NUM_SENSORS, TIMEOUT, EMITTER_PIN);
+
+unsigned int sensorValues[NUM_SENSORS]; // array of the six middle sensors
+
+LiquidCrystal lcd(22, 24, 10, 11, 12, 13); // lcd object to display pid values
+
+QTRSensorsRC qtrrcA((unsigned char[]) { A1, A2 }, // two outer sensors used to detect cross-sections (not used in tuning)
+  NUM_SENSORSA, TIMEOUT, EMITTER_PIN);
+unsigned int sensorValuesA[NUM_SENSORSA];
+
+int in1 = 4, in3 = 6, in2 = 5, in4 = 7;    // assign IN from H-Bridge to digital pin
+int ENA = A5, ENB = A0;    // assign motors to a pmw pin
+int LED = 3;  // assigns signal LED to pin 3
+int LED2 = 52, LED3 = 53;
+int KpIncrement = 34, KiIncrement = 36, KdIncrement = 38;   // assigns increment buttons to pins 34 36 38
+int incrementChange = 40; // assigns increment changer to pin 40
+int BUTTON = 12;
+int i = 0;
+
+int pos = 0;    // initialized position for PID 
+int proportional = 0;    // initialized proportional for PID
+int derivative = 0;    // initialized derivative for PID
+int lastproportional = 0;    // initialized last proportional for PID
+int integral = 0;    // initialized integral for PID
+int const qtrsetpoint = 2500;    // constant setpoint for PID
+int const sharpsetpoint = 260;
+int speedRight;    // variable for the speed of right motor
+int speedLeft;    // variable for the speed of left motor
+int max_speed = 255;    // defined maximum speed
+int error = 0;    // initialized error for PID
+float Ki = 0, Kd = 0.2, Kp = 0.05;    // define values for calculatin ERROR on PID
+float wKi = 0, wKd = 0, wKp = 0.05;  // define values for calculating error in wall follow
+
+int speedLeftBase = 100;
+int speedRightBase = 100;  // base speeds of left and right motors
+int speedLeftBaseWall = 60;
+int speedRightBaseWall = 60;  //base speeds of motors wall follow
+
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+const long decisionTimeOut = 1200;
+bool initialDrive, pidFollowTrue, decisionBool, deliveredState, phaseOne, phaseTwo, phaseThree, timeSetReverse;
+float increment[] = { 0.1, 0.01, 0.001, -0.1, -0.01, -0.001 }; // array housing increment values
+char mapChar[] = {'R', 'F', 'R', 'R', 'L', 'L', 'R', 'R', 'R', 'L', 'R', 'R', 'F', 'R', 'F', 'F', 'R'};
+byte mapCount = 0;
+bool mapDelivery[] = {true, false, true, true, false, false, true, true, false, false, true, true, false, true, false, true};
+
+void setup()
+{
+  decisionBool = true;
+  initialDrive = true;
+  pidFollowTrue = false;
+  deliveredState = false;
+  phaseOne = false;
+  phaseTwo = false;
+  phaseThree = false;
+  
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(ENA, OUTPUT); // pwm or analog
+  pinMode(ENB, OUTPUT); // pwm or analog
+  pinMode(LED, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  pinMode(KpIncrement, INPUT);
+  pinMode(KiIncrement, INPUT); // Buttons on controller are INPUTS
+  pinMode(KdIncrement, INPUT);
+  pinMode(incrementChange, INPUT);
+  lcd.begin(16,2);
+  delay(500);
+
+  digitalWrite(LED, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
+  for (int i = 0; i < 140; i++)  // make the calibration take about 4 seconds (change i for more or less)
+  {
+    qtrrc.calibrate(); //used to calibrate inner 6 sensors
+    qtrrcA.calibrate(); // used to calibrate outer sensors (not used in pid tuning)
+    delay(20);
+  }
+  digitalWrite(LED, LOW);     // turn off Arduino's LED to indicate we are through with calibration
+  delay(2000); // wait 2 seconds
+}
+
+void loop()
+{
+  while (pidFollowTrue && !initialDrive)
+    pid();
+  while (initialDrive && !pidFollowTrue)
+    initD();
+}
+
+void delivery()
+{
+  currentMillis = millis();
+  while(phaseOne)
+  {
+    pos = analogRead(A7);
+    proportional = pos - sharpsetpoint; //proportional is the position with respect to the two most inner sensors (use values ~2000-3000)
+    integral = integral + proportional; //integral adds the proportional to itself every time (area under the curve) as the position oscillates around zero, the integral will balance itself out
+    derivative = proportional - lastproportional; //  derivative will take the slope of the oscillation per cycle
+    error = (proportional*wKp + integral*wKi + derivative*wKd); //error is computed by the sum of the following pid values, each with their own constants
+    lastproportional = proportional; //records the current positon for the next cycle
+    
+    speedLeft = speedLeftBase - error; //left motor speed relative to base speed minus error
+    speedRight = speedRightBase + error; //right motor speed relative to base speed plus error
+
+
+    if (speedRight>max_speed) //used to cap off motor at max speed
+      speedRight = max_speed;
+    if (speedLeft > max_speed)
+      speedLeft = max_speed;
+
+    if (speedRight< 0) speedRight = 0; //keep speed from going negative
+    if (speedLeft < 0) speedLeft = 0;
+  
+    digitalWrite(in1, LOW); //in1-in4 are used to determine direction of motor spin and depends on the wiring
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+
+    analogWrite(ENA, speedRight); //finally the motor speeds are assigned to the motors through the H-Bridge
+    analogWrite(ENB, speedLeft);
+    if(digitalRead(BUTTON) == LOW)
+      buttonState = true;
+    if(buttonState)
+    {
+      buttonState = false;
+      phaseTwo = true;
+      phaseOne = false;
+    }
+  }
+  
+  while(phaseTwo)
+  {
+    
+    digitalWrite(spiral, HIGH);
+    
+    if(digitalRead(deliverySensor) == HIGH)
+    {
+      phaseThree = true;
+      phaseTwo = false;
+      timeSetReverse = true;
+    }
+  }
+  
+  while(phaseThree)
+  {
+    if(timeSetReverse)
+    {
+      previousMillis = millis();
+      timeSetReverse = false;
+    }
+    currentMillis = millis();
+    
+    pos = analogRead(A7);
+    proportional = pos - sharpsetpoint; //proportional is the position with respect to the two most inner sensors (use values ~2000-3000)
+    integral = integral + proportional; //integral adds the proportional to itself every time (area under the curve) as the position oscillates around zero, the integral will balance itself out
+    derivative = proportional - lastproportional; //  derivative will take the slope of the oscillation per cycle
+    error = (proportional*wKp + integral*wKi + derivative*wKd); //error is computed by the sum of the following pid values, each with their own constants
+    lastproportional = proportional; //records the current positon for the next cycle
+  
+    speedLeft = speedLeftBase - error; //left motor speed relative to base speed minus error
+    speedRight = speedRightBase + error; //right motor speed relative to base speed plus error
+
+
+    if (speedRight>max_speed) //used to cap off motor at max speed
+      speedRight = max_speed;
+    if (speedLeft > max_speed)
+      speedLeft = max_speed;
+
+    if (speedRight< 0) speedRight = 0; //keep speed from going negative
+    if (speedLeft < 0) speedLeft = 0;
+  
+    digitalWrite(in1, HIGH); //in1-in4 are used to determine direction of motor spin and depends on the wiring
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+
+    analogWrite(ENA, speedRight); //finally the motor speeds are assigned to the motors through the H-Bridge
+    analogWrite(ENB, speedLeft);
+    
+    if(currentMillis - previousMillis >= 3000)
+    {
+      phaseThree = false;
+      deliveredState = true;
+    }
+  }
+}
+
+void pid() //main line follow code
+{
+  //stats(); //stats will print out the Kp, Ki, Kd, and increment values
+ // calculateError(); //stores error value in error
+  currentMillis = millis();
+ 
+  qtrrcA.readCalibrated(sensorValuesA); //read input of outer sensors (0-1000)
+  pos = qtrrc.readLine(sensorValues); //readLine function returns the position of the robot on the line
+  proportional = pos - qtrsetpoint; //proportional is the position with respect to the two most inner sensors (use values ~2000-3000)
+  integral = integral + proportional; //integral adds the proportional to itself every time (area under the curve) as the position oscillates around zero, the integral will balance itself out
+  derivative = proportional - lastproportional; //  derivative will take the slope of the oscillation per cycle
+  error = (proportional*Kp + integral*Ki + derivative*Kd); //error is computed by the sum of the following pid values, each with their own constants
+  lastproportional = proportional; //records the current positon for the next cycle
+  
+  if (digitalRead(KpIncrement) == HIGH)
+    Kp += increment[i];
+  if (digitalRead(KiIncrement) == HIGH) //conditionals are used to increment Kp,Kd,Ki according to button pressed
+    Ki += increment[i];                       // value changes according to 'i'
+  if (digitalRead(KdIncrement) == HIGH)
+    Kd += increment[i];
+
+ if (digitalRead(incrementChange) == HIGH)                                   // used to change the incrementing value
+ {
+   if (i == 5) //once i becomes 5 (array location 6)
+     i = 0; //return i to beginning of array (location 1)
+   else
+     i++; //otherwise increment i
+ }
+ if(currentMillis - previousMillis >= decisionTimeOut)
+ {
+  
+  decisionBool = true;
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+ }
+ 
+ if((sensorValuesA[0] > 700 || sensorValuesA[1] > 700)&& decisionBool)
+ {
+    decision();
+ }
+ 
+ lcd.setCursor(0, 0); lcd.print("p:");
+ lcd.print(Kp,DEC); 
+ lcd.setCursor(7,0); lcd.print("  d:"); lcd.print(Kd, DEC);
+ lcd.setCursor(0, 1); lcd.print("i:"); lcd.print(Ki, DEC);
+ lcd.setCursor(7,1); lcd.print(" n:");
+ speedLeft = speedLeftBase - error; //left motor speed relative to base speed minus error
+ speedRight = speedRightBase + error; //right motor speed relative to base speed plus error
+
+
+  if (speedRight>max_speed) //used to cap off motor at max speed
+    speedRight = max_speed;
+  if (speedLeft > max_speed)
+    speedLeft = max_speed;
+
+  if (speedRight< 0) speedRight = 0; //keep speed from going negative
+  if (speedLeft < 0) speedLeft = 0;
+
+  digitalWrite(in1, LOW); //in1-in4 are used to determine direction of motor spin and depends on the wiring
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  
+  analogWrite(ENA, speedRight); //finally the motor speeds are assigned to the motors through the H-Bridge
+  analogWrite(ENB, speedLeft);
+  //assignMotorSpeedPID(error);
+}
+
+void stats() //used to display values on an LCD
+{
+ lcd.setCursor(0, 0); lcd.print("hello:");
+ lcd.print(Kp,DEC); 
+ lcd.setCursor(7,0); lcd.print("  d:"); lcd.print(Kd, DEC);
+ lcd.setCursor(0, 1); lcd.print("i:"); lcd.print(Ki,DEC);
+ lcd.setCursor(7,1); lcd.print(" n:");
+ switch(i)
+ {
+    case 0: lcd.print("0.1"); break;
+    case 1: lcd.print("0.01"); break;
+    case 2: lcd.print("0.001"); break;
+    case 3: lcd.print("-0.1"); break;
+    case 4: lcd.print("-0.01"); break;
+    case 5: lcd.print("-0.001"); break;
+    default:;
+ }
+}
+
+void initD()
+{
+  qtrrc.readCalibrated(sensorValues);
+  pos = analogRead(A7);
+  proportional = pos - sharpsetpoint; //proportional is the position with respect to the two most inner sensors (use values ~2000-3000)
+  integral = integral + proportional; //integral adds the proportional to itself every time (area under the curve) as the position oscillates around zero, the integral will balance itself out
+  derivative = proportional - lastproportional; //  derivative will take the slope of the oscillation per cycle
+  error = (proportional*wKp + integral*wKi + derivative*wKd); //error is computed by the sum of the following pid values, each with their own constants
+  lastproportional = proportional; //records the current positon for the next cycle
+    
+  speedLeft = speedLeftBase - error; //left motor speed relative to base speed minus error
+  speedRight = speedRightBase + error; //right motor speed relative to base speed plus error
+
+
+  if (speedRight>max_speed) //used to cap off motor at max speed
+    speedRight = max_speed;
+  if (speedLeft > max_speed)
+    speedLeft = max_speed;
+
+  if (speedRight< 0) speedRight = 0; //keep speed from going negative
+  if (speedLeft < 0) speedLeft = 0;
+  
+  digitalWrite(in1, LOW); //in1-in4 are used to determine direction of motor spin and depends on the wiring
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  analogWrite(ENA, speedRight); //finally the motor speeds are assigned to the motors through the H-Bridge
+  analogWrite(ENB, speedLeft);
+  
+  if(sensorValues[2] > 700 || sensorValues[3] > 700)
+  {
+    analogWrite(ENA, speedRightBase+60);
+    analogWrite(ENB, speedLeftBase);
+    delay(200);
+    pidFollowTrue = true;
+    initialDrive = false;
+  }
+  
+}
+
+void calculateError()
+{
+  qtrrcA.readCalibrated(sensorValuesA); //read input of outer sensors (0-1000)
+  pos = qtrrc.readLine(sensorValues); //readLine function returns the position of the robot on the line
+  proportional = pos - setpoint; //proportional is the position with respect to the two most inner sensors (use values ~2000-3000)
+  integral = integral + proportional; //integral adds the proportional to itself every time (area under the curve) as the position oscillates around zero, the integral will balance itself out
+  derivative = proportional - lastproportional; //  derivative will take the slope of the oscillation per cycle
+  error = (proportional*Kp + integral*Ki + derivative*Kd); //error is computed by the sum of the following pid values, each with their own constants
+  lastproportional = proportional; //records the current positon for the next cycle
+}
+
+void assignMotorSpeedPID(int e)
+{
+  speedLeft = speedLeftBase - e; //left motor speed relative to base speed minus error
+  speedRight = speedRightBase + e; //right motor speed relative to base speed plus error
+
+
+  if (speedRight>max_speed) //used to cap off motor at max speed
+    speedRight = max_speed;
+  if (speedLeft > max_speed)
+    speedLeft = max_speed;
+
+  if (speedRight< 0) speedRight = 0; //keep speed from going negative
+  if (speedLeft < 0) speedLeft = 0;
+
+  digitalWrite(in1, LOW); //in1-in4 are used to determine direction of motor spin and depends on the wiring
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  analogWrite(ENA, speedRight); //finally the motor speeds are assigned to the motors through the H-Bridge
+  analogWrite(ENB, speedLeft);
+}
+
+void decision()
+{
+  if(!mapDelivery[mapCount] || deliveredState)
+  {
+    if(mapChar[mapCount] == 'R')
+    {
+      digitalWrite(LED, HIGH);
+      analogWrite(ENA, 0); //finally the motor speeds are assigned to the motors through the H-Bridge
+      analogWrite(ENB, speedLeftBase);
+      delay(500);
+      digitalWrite(LED,LOW);
+      decisionBool = false;
+      previousMillis = currentMillis;
+      digitalWrite(LED2, HIGH);
+      deliveredState = false;
+      if(mapCount == 16)
+        mapCount = 0;
+      else
+        ++mapCount;
+      deliveredState = false;
+    }
+    else if(mapChar[mapCount] == 'L'|| )
+    {
+      digitalWrite(LED, HIGH);
+      analogWrite(ENA, speedRightBase+60); //finally the motor speeds are assigned to the motors through the H-Bridge
+      analogWrite(ENB, 0);
+      delay(500);
+      digitalWrite(LED,LOW);
+      decisionBool = false;
+      previousMillis = currentMillis;
+      digitalWrite(LED2, HIGH);
+      deliveredState = false;
+      if(mapCount == 16)
+        mapCount = 0;
+      else
+        ++mapCount;
+      deliveredState = false;
+    }
+    else if(mapChar[mapCount] == 'F')
+    {
+      digitalWrite(LED, HIGH);
+      analogWrite(ENA, 0); //finally the motor speeds are assigned to the motors through the H-Bridge
+      analogWrite(ENB, 0);
+      delay(200);
+      proportional = 0;
+      digitalWrite(LED,LOW);
+      decisionBool = false;
+      previousMillis = currentMillis;
+      digitalWrite(LED3, HIGH);
+      deliveredState = false;
+      if(mapCount == 16)
+        mapCount = 0;
+      else
+        ++mapCount;
+      deliveredState = false;
+    }
+  }
+  
+  else
+  {
+    phaseOne = true;
+    delivery();
+  }
+}
